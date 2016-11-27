3dda2a020a3c0fdd21a6b3aba5a80b4f510010d1
EverCode-Dave <evercode.dave@gmail.com>
2016-11-27 11:28:14 +0000

    Adds better console logging

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from evercode_os import run_command
 # Store the setup version number.
 setup_version_number = "1.0.1"
 
-# Begin by writing some version information to file.
-run_command("Saving version number", "echo 'EverCode Server from version %s' > /evercode.version" % setup_version_number)
+# Save the version number to root.
+run_command("Saving version number...", "echo 'EverCode Server from version %s' > /evercode.version" % setup_version_number)
 
-# Set execute rights on any shell scripts in the folder.
-run_command("Setting execute rights on shell scripts", "chmod u+x *.sh")
+# Set execute rights on shell scripts.
+run_command("Setting execute rights on shell scripts...", "chmod u+x *.sh")
+
+# Add a symlink to "/var/www/html".
+run_command("Adding symlink to public web folder...", "ln -s /var/www/html /root/html")
--- sync.py
+++ sync.py
@@ -1,4 +1,5 @@
 from evercode_os import run_command
 
-# Synchronise with GitHub.
-run_command("Synchronising with GitHub", "git pull")
+# Synchronise with GitHub - useful for testing new scripts without a complete
+# server wipe and reset.
+run_command("Synchronising with GitHub...", "git pull")
