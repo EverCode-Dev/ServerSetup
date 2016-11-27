529e68cb0985b280937f25b11ab5cdf5d52ba62f
EverCode-Dave <evercode.dave@gmail.com>
2016-11-27 11:38:01 +0000

    Fixes no repository found error

--- sync.py
+++ sync.py
@@ -1,4 +1,11 @@
+# Import the run_command function.
 from evercode_os import run_command
+
+# Import chdir from os to change the CWD.
+from os import chdir
+
+# Set the CWD to the EverCode Server Setup directory.
+chdir("/root/evercode-setup")
 
 # Synchronise with GitHub - useful for testing new scripts without a complete
 # server wipe and reset.
