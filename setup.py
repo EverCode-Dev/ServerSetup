c20ab1771db91bf15beb9c2bb036a11c10f43084
EverCode <git@evercode.co>
2016-11-27 11:18:38 +0100

    Add version variable.

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 # Import the OS module.
 import os
 
+# Store the setup version number.
+setup_version_number = "1.0.1"
+
 # Begin by writing some version information to file.
+
 os.system("echo 'EverCode Server from version 1.0.1' > /evercode.version")
