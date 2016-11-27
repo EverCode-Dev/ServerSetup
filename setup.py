70e7b5395a8b38ca3e9e4ff0d68aaccd8506377e
EverCode-Dave <evercode.dave@gmail.com>
2016-11-27 11:00:38 +0000

    Adds command to set execute rights on shell scripts

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ import os
 setup_version_number = "1.0.1"
 
 # Begin by writing some version information to file.
-
+print("Saving version number.")
 os.system("echo 'EverCode Server from version 1.0.1' > /evercode.version")
+
+# Set execute rights on any shell scripts in the folder.
+print("Setting execute rights on shell scripts.")
+os.system("chmod +x *.sh")
