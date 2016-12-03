e45f43cc25b9de88894f8a8c16cf3225180163b6
EverCode <git@evercode.co>
2016-12-03 12:51:28 +0100

    Added method of altering Git ignore over FTP

---
+++ push-ignore.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -fv gitignore .gitignore
+git add .gitignore
+git commit -m 'Updated Git ignore file'
+git push https://EverCode-Dave:EffortCourse602@github.com/EverCode-Dev/ServerSetup.git
